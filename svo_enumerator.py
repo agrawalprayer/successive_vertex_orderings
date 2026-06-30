b0258c52efb3f58645baa484f7fc19c9b44eb184
Prarthana Agrawal <68094239+agrawalprayer@users.noreply.github.com>
2026-06-30 11:53:11 +0100

    Created svo_enumerator file

---
+++ svo_enumerator.py
@@ -0,0 +1,250 @@
+"""Enumerate successive vertex orderings of a finite simple graph.
+
+This module implements the inclusion--exclusion method described in the paper "Successive vertex orderings of graphs".
+
+Mathematical input:
+    - a finite simple graph G given by a 0/1 adjacency matrix A
+
+Core identity used by the implementation:
+    sigma(G) / n! = sum_{I independent} (-1)^{|I|} * a(I)/n * b(I)
+
+where
+    a(I) = n - |N[I]|,
+    b(∅) = 1,
+    b(I) = (1 / |N[I]|) * sum_{v in I} b(I minus {v}) for I != ∅.
+
+Author: Hadi, Prarthana
+Date: 09 Feb 2026
+Version: 1.0
+"""
+
+# Import packages
+from __future__ import annotations
+from typing import Union, Iterable
+from fractions import Fraction
+import math
+
+class Subset:
+    vector: list[int]
+    n: int
+
+    def __init__(self, vector: list[int]):
+        self.vector = vector
+        for i in vector:
+            assert i == 0 or i == 1
+        self.n = len(vector)
+
+    def __len__(self):
+        return self.n
+
+    def add_element(self, pos: int):
+        """Add element to pos."""
+        assert 0 <= pos < self.n
+        self.vector[pos] = 1
+
+    def remove_element(self, pos: int):
+        """Remove element at pos."""
+        assert 0 <= pos < self.n
+        self.vector[pos] = 0
+
+    def __add__(self, other: Subset) -> Subset:
+        """Gives the union of two subsets."""
+        assert self.n == len(other)
+        result_list = [self.vector[i] | other.vector[i] for i in range(self.n)]
+        return Subset(result_list)
+
+    def __mul__(self, other: Subset) -> Subset:
+        """Gives the intersection of two subsets."""
+        assert self.n == len(other)
+        result_list = [self.vector[i] & other.vector[i] for i in range(self.n)]
+        return Subset(result_list)
+
+    def __abs__(self) -> int:
+        """Returns size of subset."""
+        return sum(self.vector)
+
+    def get_children(self) -> list[Subset]:
+        """Get all subsets with one more element."""
+        children = []
+        for i in range(self.n):
+            if self.vector[i] == 0:
+                child = Subset(self.vector.copy())
+                child.add_element(i)
+                children.append(child)
+        return children
+
+    def get_parents(self) -> list[Subset]:
+        """Get all subsets with one less element."""
+        parents = []
+        for i in range(self.n):
+            if self.vector[i] == 1:
+                parent = Subset(self.vector.copy())
+                parent.remove_element(i)
+                parents.append(parent)
+        return parents
+
+    def __getitem__(self, i: int):
+        return self.vector[i]
+
+    def __setitem__(self, i: int, x: int):
+        assert x == 0 or x == 1
+        self.vector[i] = x
+
+    @property
+    def largest_element(self) -> int:
+        if abs(self) == 0:
+            return -1
+        else:
+            return max([i for i in range(self.n) if self.vector[i] == 1])
+
+    def get_next_subsets(self) -> list[Subset]:
+        """Get all children subsets with one element added to right."""
+        children = []
+        largest_element = self.largest_element
+        for i in range(largest_element + 1, self.n):
+            child = Subset(self.vector.copy())
+            child.add_element(i)
+            children.append(child)
+        return children
+
+    def __eq__(self, other) -> bool:
+        return (tuple(self.vector) == tuple(other.vector))
+
+    def __hash__(self) -> int:
+        return hash(tuple(self.vector))
+
+    def __str__(self) -> str:
+        string = ''
+        for i in range(self.n):
+            string += str(self.vector[i])
+        return string
+
+    def __repr__(self) -> str:
+        return str(self)
+
+def make_empty_subset(n: int) -> Subset:
+    return Subset([0 for i in range(n)])
+
+class Polyomino:
+    adj_matrix: list[list[int]]
+    n: int
+
+    def __init__(self, adj_matrix: list[list[int]]):
+        self.n = len(adj_matrix)
+        for i in range(self.n):
+            assert len(adj_matrix[i]) == self.n
+            for j in range(self.n):
+                assert adj_matrix[i][j] == 0 or adj_matrix[i][j] == 1
+        self.adj_matrix = adj_matrix
+
+    def neighborhood(self, subset: Subset) -> Subset:
+        neighborhood = make_empty_subset(self.n)
+        for i in range(self.n):
+            if subset[i] == 1:
+                neighborhood += Subset(self.adj_matrix[i])
+        return neighborhood
+
+    def __mul__(self, subset: Subset) -> Subset:
+        return self.neighborhood(subset)
+
+    def is_independent(self, subset: Subset) -> bool:
+        return abs((self * subset) * subset) == 0
+
+    def __xor__(self, subset: Subset) -> bool:
+        return self.is_independent(subset)
+
+class Node:
+    independent_set: Subset
+    neighborhood: Subset
+    n: int # polyomino size
+    a: int # |V| - |N(I) + I|
+    b: Fraction | None # parameter to be computed
+    d: int # subset size
+    children: list[Node]
+    parents: list[Node]
+    is_root: bool
+
+    def __init__(self, subset: Subset, neighborhood: Subset):
+        assert subset.n == neighborhood.n
+        assert abs(subset * neighborhood) == 0
+        self.independent_set = subset
+        self.neighborhood = neighborhood
+        self.n = len(self.independent_set)
+        self.a = self.n - abs(neighborhood) - abs(self.independent_set)
+        self.d = abs(self.independent_set)
+        self.children = []
+        self.parents = []
+        if abs(self.independent_set) == 0:
+            self.is_root = True
+            self.b = 1
+        else:
+            self.is_root = False
+            self.b = None # to be calculated
+
+    def __eq__(self, other) -> bool:
+        return self.independent_set == other.independent_set
+
+    def __hash__(self) -> int:
+        return hash(self.independent_set)
+
+    @property
+    def value(self) -> Fraction:
+        assert self.b is not None, "b not defined yet"
+        return Fraction(self.a) * Fraction(self.b) * Fraction(1, self.n) * (-1) ** self.d
+
+    def calculate_b(self) -> Fraction:
+        self.b = Fraction(0)
+        for parent in self.parents:
+            self.b += parent.b / Fraction(self.n - self.a)
+        return self.b
+    
+
+class Lattice:
+    n: int # polyomino size
+    polyomino: Polyomino
+    root: Node
+    levels: dict[int, list[Node]] # returns nodes with that many elements
+    nodes: dict[int, Node] # key is hash value of independent set tuple
+
+    def __init__(self, polyomino: Polyomino):
+        self.n = polyomino.n
+        self.polyomino = polyomino
+        self.root = Node(make_empty_subset(self.n), make_empty_subset(self.n))
+        self.levels = {0: [self.root]}
+        self.nodes = {make_empty_subset(self.n): self.root}
+        for d in range(1, self.n):
+            self.levels[d] = []
+            for node in self.levels[d - 1]:
+                next_subsets = node.independent_set.get_next_subsets()
+                for subset in next_subsets:
+                    if not self.polyomino ^ subset:
+                        continue
+                    new_neighborhood = self.polyomino * node.independent_set + Subset(self.polyomino.adj_matrix[subset.largest_element])
+                    new_node = Node(subset, new_neighborhood)
+                    self.levels[d].append(new_node)
+                    self.nodes[new_node.independent_set] = new_node
+                    new_node.parents = [self.nodes[parent] for parent in new_node.independent_set.get_parents()]
+                    for parent_node in new_node.parents:
+                        parent_node.children.append(new_node)
+                    new_node.calculate_b()
+            if self.levels[d] == []:
+                self.levels.pop(d)
+                break
+
+    def sum_all_values(self) -> Fraction:
+        total_sum = Fraction(0)
+        for node in self.nodes.values():
+            total_sum += node.value
+        return total_sum
+
+#------------------------------------------------------------------------#
+def successive_vertex_orderings(adj_matrix):
+    """
+    Given a graph (adjacency matrix), count the number of distinct growth sequences (successive vertex orderings).
+    """
+
+    polyomino = Polyomino(adj_matrix)
+    lattice = Lattice(polyomino)
+    fraction = lattice.sum_all_values()
+
+    return int(fraction*math.factorial(polyomino.n))
